9bd591963857bbbcf920bab87493e8c52e4485f3
Vaishali Kolhe <55869769+VaishaliKolhe@users.noreply.github.com>
2019-11-20 21:45:42 +0530

    load_display_save

---
+++ Load_Display_Save/load_display_save.py
@@ -0,0 +1,13 @@
+import cv2  # Importing computer vision library
+import argparse   # used for command line arguments
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+# To get image argument and its path in a dictionary
+args = vars(ap.parse_args())
+print(args)
+
+image = cv2.imread(args["image"])  # to read images
+cv2.imshow("Image", image)  # to display images
+cv2.waitKey(0)
+cv2.imwrite("newimage.jpg", image)  # to write into new images
